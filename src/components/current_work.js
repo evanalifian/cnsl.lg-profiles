4d5d2b4c8e2b0e5da7b1af95568eee34fdd9b23c
evanalifian <evanalifian@gmail.com>
2026-05-04 21:00:50 +0700

    feat: add link to github repository and display repo description

--- src/components/current_work.js
+++ src/components/current_work.js
@@ -37,27 +37,29 @@ export default async function currentWork() {
         </div>
       </div>
 
-      <div class="border border-zinc-900 p-5 group-hover:border-zinc-700 transition-colors bg-[#020202]">
+      <a href="https://github.com/evanalifian/${pushEvent.repo.name.slice(12)}" target="_blank">
+        <div class="border border-zinc-900 p-5 group-hover:border-zinc-700 transition-colors bg-[#020202]">
         
-        <div class="flex justify-between items-start mb-4">
-          <h3 class="text-sm font-bold tracking-widest">
-            ${repoData?.full_name || repoFullName}
-          </h3>
+          <div class="flex justify-between items-start mb-4">
+            <h3 class="text-sm font-bold tracking-widest">
+              ${repoData?.full_name || repoFullName}
+            </h3>
 
-          <span class="text-[10px] font-mono text-zinc-500">
-            ${repoData?.private ? "PRIVATE" : "PUBLIC"}
-          </span>
+            <span class="text-[10px] font-mono text-zinc-500">
+              ${repoData?.private ? "PRIVATE" : "PUBLIC"}
+            </span>
+          </div>
+
+          <div class="w-full h-0.5 bg-zinc-900 relative">
+            <div class="absolute top-0 left-0 h-full bg-white w-full shadow-[0_0_8px_rgba(255,255,255,0.3)] animate-pulse"></div>
+          </div>
+
+          <p class="text-[10px] text-zinc-500 mt-4 leading-normal italic">
+            "${description}"
+          </p>
+
         </div>
-
-        <div class="w-full h-0.5 bg-zinc-900 relative">
-          <div class="absolute top-0 left-0 h-full bg-white w-full shadow-[0_0_8px_rgba(255,255,255,0.3)] animate-pulse"></div>
-        </div>
-
-        <p class="text-[10px] text-zinc-500 mt-4 leading-normal italic">
-          "${description}"
-        </p>
-
-      </div>
+      </a>
     </section>
   `;
 }
